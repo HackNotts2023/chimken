05eaa687cb760a8d965a2335e50e537603b78bd8
Carnagion <carnagion.games@gmail.com>
2023-02-11 10:31:35 +0000

    Add initial code

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,16 @@
-fn main() {
+use anyhow::Result as AnyResult;
+
+use axum::Router;
+use axum::Server;
+
+#[tokio::main]
+async fn main() -> AnyResult<()> {
+    let app = Router::new()
+        .into_make_service();
+
+    Server::bind(&"0.0.0.0:3000".parse()?)
+        .serve(app)
+        .await?;
+    
+    Ok(())
 }
