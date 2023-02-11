46d8f20d32a2aa869ee1ccb1abac286aa6e63718
Carnagion <carnagion.games@gmail.com>
2023-02-11 18:54:06 +0000

    Use static paths when referring to CSS

--- src/lib.rs
+++ src/lib.rs
@@ -12,5 +12,6 @@ pub mod ui;
 async fn axum() -> ShuttleAxum {
     Ok(SyncWrapper::new(Router::new()
         .route("/", routing::get(ui::index))
-        .route("/static/*path", routing::get(ui::statics))))
+        .route("/*path", routing::get(ui::template_path))
+        .route("/static/*path", routing::get(ui::static_path))))
 }
