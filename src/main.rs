52d545db837f156950e5be54ae13de3016a932a7
Carnagion <carnagion.games@gmail.com>
2023-02-11 10:37:08 +0000

    Add initial index page

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ use anyhow::Result as AnyResult;
 
 use axum::Router;
 use axum::Server;
+use axum::routing;
 
 #[tokio::main]
 async fn main() -> AnyResult<()> {
     let app = Router::new()
+        .route("/", routing::get(hnproject::index))
         .into_make_service();
 
     Server::bind(&"0.0.0.0:3000".parse()?)
