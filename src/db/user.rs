a836844ffb79a0ac8348f02a0912ae528ea36b2c
Isaac <isaacdestura@icloud.com>
2023-02-11 16:13:20 +0000

    adding functions to user to query the user collection for its documents

--- src/db/user.rs
+++ src/db/user.rs
@@ -3,6 +3,17 @@ use serde::Serialize;
 
 use crate::db::Id;
 
+use mongodb::Client;
+use mongodb::Cursor;
+use mongodb::bson;
+use mongodb::error::Error as DbError;
+
+use crate::db::DATABASE;
+use crate::db::Comment;
+
+use super::Restaurant;
+
+
 const USER_COLLECTION: &str = "users";
 
 #[derive(Debug, Clone, Eq, Serialize, Deserialize)]
@@ -18,3 +29,61 @@ pub struct User {
 }
 
 impl_unique!(User => USER_COLLECTION);
+
+impl User {
+    pub async fn favorites(&self, client: &Client) -> Result<Cursor<Restaurant>, DbError> {
+        client.database(DATABASE)
+            .collection(USER_COLLECTION)
+            .find(bson::doc! {
+                "_id": {
+                    "$in": &self.favorite_ids,
+                },
+            }, None)
+            .await
+    }
+
+    pub async fn munch_list(&self, client: &Client) -> Result<Cursor<User>, DbError> {
+        client.database(DATABASE)
+            .collection(USER_COLLECTION)
+            .find(bson::doc! {
+                "_id": {
+                    "$in": &self.munch_list_ids,
+                },
+            }, None)
+            .await
+    }
+
+    pub async fn following_list(&self, client: &Client) -> Result<Cursor<User>, DbError> {
+        client.database(DATABASE)
+            .collection(USER_COLLECTION)
+            .find(bson::doc! {
+                "_id": {
+                    "$in": &self.following_ids,
+                },
+            }, None)
+            .await
+    }
+
+    pub async fn followers_list(&self, client: &Client) -> Result<Cursor<User>, DbError> {
+        client.database(DATABASE)
+            .collection(USER_COLLECTION)
+            .find(bson::doc! {
+                "_id": {
+                    "$in": &self.follower_ids,
+                },
+            }, None)
+            .await
+    }
+
+    pub async fn comments(&self, client: &Client) -> Result<Cursor<Comment>, DbError> {
+        client.database(DATABASE)
+            .collection(USER_COLLECTION)
+            .find(bson::doc! {
+                "_id": {
+                    "$in": &self.comment_ids,
+                },
+            }, None)
+            .await
+    }
+
+}
